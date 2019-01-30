d49830d2efde1c5189564f1e4ac4892091650eb4
wierzcho <s13169@pjwstk.edu.pl>
2019-01-30 20:09:11 +0100

    Added customized admin sites

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@admin.com",
+            password="password123",
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@user.com",
+            password="password123",
+            name="Test user full name",
+        )
+
+    def test_users_listed(self):
+        url = reverse("admin:core_user_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
+    def test_user_change_page(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse("admin:core_user_add")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
